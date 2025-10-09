6a2f848294ca4771f9be701ecbba91f6ea25fbf0
RAMEES-KKVL <rameesp41750@gmail.com>
2025-10-09 17:43:12 +0530

    modified image loading bug

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -95,6 +95,7 @@ const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
 
   const result = await payload.find({
     collection: 'pages',
+    depth: 1,
     draft,
     limit: 1,
     pagination: false,
